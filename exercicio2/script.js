ebc784e49e0a66cab3a008106a28eac267c2ec72
falcaomm <manu.m.falcao@gmail.com>
2022-08-04 18:29:24 -0300

    Exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 17
+let terminouEM = false
+let CursandoFaculdade = false
 
-if(){
-
+if(idade>=18){
+    console.log("É maior de idade");
 }else{
-	
+    console.log("Não é maior de idade");
+}
+
+if(terminouEM){
+    console.log("Terminou o EM");
+}else{
+    console.log("Não terminou o EM");
+}
+
+if(CursandoFaculdade){
+    console.log("Cursando faculdade");
+}else{
+    console.log("Não está cursando faculdade");
 }
